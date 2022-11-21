03f2b2c7a5ae34559ca7df08e12ca8bc8da74543
DalPenny <105093756+DalPenny@users.noreply.github.com>
2022-11-21 14:23:28 -0600

    adding navigational tabs with styles

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,57 +1,59 @@
 import React from 'react';
+import '../styles/NavTabs.css';
 
 // TODO: Add a comment explaining how we are able to extract the key value pairs from props
 // destructering
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#aboutme"
-          onClick={() => handlePageChange('AboutMe')}
-          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
-          // ternary operator is our current tab active
-          className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
-        >
-          About Me
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#portfolio"
-          onClick={() => handlePageChange('Portfolio')}
-          //  TODO: Add a comment explaining what this logic is doing
-          //
-
-          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#resume"
-          onClick={() => handlePageChange('Resume')}
-          //  TODO: Add a comment explaining what this logic is doing
-
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          //  TODO: Add a comment explaining what this logic is doing
-         // setting page name, classes will be updated, state is updated
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+    <div class="grid-container flex-row">
+      <div class="item1">
+        <p class="p">Charitha Alwis</p></div>
+      <nav>
+        <div class="item1">
+          <ul className="nav nav-tabs">
+            <li className="nav-item">
+              <a
+                href="#aboutme"
+                onClick={() => handlePageChange('AboutMe')}
+                // ternary operator is our current tab active
+                className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
+              >
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#portfolio"
+                onClick={() => handlePageChange('Portfolio')}
+                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#resume"
+                onClick={() => handlePageChange('Resume')}
+                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+              >
+                Resume
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                href="#contact"
+                // setting page name, classes will be updated, state is updated
+                onClick={() => handlePageChange('Contact')}
+                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 }
 
