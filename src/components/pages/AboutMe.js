baf68c657a011953a11450a073be9acb1eb3cfb7
Charitha Alwis <sathruvani@gmail.com>
2022-12-19 13:20:52 -0600

    trying to push to heroku again

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 import '../../styles/AboutMe.css';
 import myimage from '../../assets/images/charitha.jpg';
 export default function AboutMe() {
